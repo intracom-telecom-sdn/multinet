49cb95067d3738f5b364a895f759a691e068ec27
panageo <panageo@gmail.com>
2016-08-25 15:43:22 +0300

    multinet test

--- net/multinet.py
+++ net/multinet.py
@@ -352,16 +352,16 @@ class Multinet(mininet.net.Mininet):
 
         while (time.time() - transmission_start) <= traffic_transmission_interval:
             src_mac, dst_mac = self.generate_mac_address_pairs(current_mac)
-            src_ip = self.long2ip(current_ip)
-            dst_ip = self.long2ip(current_ip + 1)
+            arp_ip_h1 = self.long2ip(current_ip)
+            arp_ip_h2 = self.long2ip(current_ip + 1)
             current_mac = hex(int(current_mac, 16) + 2)
             current_ip += 2
             self.hosts[host_index].sendCmd(
                 'sudo mz -a {0} -b {1} -t arp "targetmac={2}, sendermac={3}, targetip={4}, senderip={5}"'.
-                format(src_mac, dst_mac, dst_mac, src_mac, dst_ip, src_ip))
+                format('ff:ff:ff:ff:ff:ff', dst_mac, dst_mac, src_mac, arp_ip_h1, arp_ip_h1))
             self.hosts[host_index + 1].sendCmd(
                 'sudo mz -a {0} -b {1} -t arp "targetmac={2}, sendermac={3}, targetip={4}, senderip={5}"'.
-                format(dst_mac, src_mac, src_mac, dst_mac, src_ip, dst_ip))
+                format('ff:ff:ff:ff:ff:ff', src_mac, src_mac, dst_mac, arp_ip_h2, arp_ip_h2))
             time.sleep(traffic_transmission_delay)
             host_index += self._hosts_per_switch
 
