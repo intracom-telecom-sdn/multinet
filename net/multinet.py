55f847d9459cb30207d3bc36a62b7ae7a70b2fea
panageo <panageo@gmail.com>
2016-09-05 17:00:09 +0300

    add debug message

--- net/multinet.py
+++ net/multinet.py
@@ -300,6 +300,7 @@ class Multinet(mininet.net.Mininet):
         flow_number_total = 0
         t_start = time.time()
         for switch in self.switches:
+            logging.debug('[get_flows] Raw command output' + switch.dpctl('-O OpenFlow13 dump-aggregate'))
             for stat_item in switch.dpctl('-O OpenFlow13 dump-aggregate').split(' ')[-1]:
                 if stat_item.split('=')[0] == 'flow_count' and len(stat_item.split('=')) == 2:
                     flow_number_total += int(stat_item.split('=')[-1])
