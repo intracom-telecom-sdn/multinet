b448fb621d4a4580c3dcb9990870d1164685bd3a
Konstantinos Papadopoulos <konpap@intracom-telecom.com>
2016-11-22 11:20:48 +0200

    Restored venv handlers

--- bin/venv_handler_master.sh
+++ bin/venv_handler_master.sh
@@ -15,10 +15,8 @@
 
 if [ "$#" -eq 3 ]
 then
-    source /opt/venv_multinet/bin/activate
-    PYTHONPATH=$1
-    echo 'python $2 --json-config $3'
-    python $2 --json-config $3
+	echo 'source /opt/venv_multinet/bin/activate; PYTHONPATH=$1 python $2 --json-config $3'
+    source /opt/venv_multinet/bin/activate; PYTHONPATH=$1 python $2 --json-config $3
 else
     echo "Invalid number of arguments."
     exit 1
